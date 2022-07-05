04fe25e1dd0d3abd2197ff0b1de455180b0a01f3
BabaikaRub <rodionov.mihail02@mail.ru>
2022-07-05 22:01:33 +0300

    create class Parce and add GUI

--- parce.py
+++ parce.py
@@ -1,80 +1,103 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+from PyQt5 import QtWidgets
 
+import sys
 import csv
 import math
 
-
-ua = UserAgent()
-
-headers = {
-    'user-agent': ua.random,
-    'accept': 'application/json, text/plain, */*',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-}
-
-# Список городов
-cities = ['moskva', 'spb', 'barnaul', 'vladivostok', 'volgograd', 'voronezh', 'ekaterinburg', 'izhevsk', 'kazan',
-          'krasnodar', 'krasnoyarsk', 'nnovgorod', 'novosibirsk', 'omsk', 'perm', 'rostov-na-donu', 'samara', 'saratov',
-          'tolyatti', 'ulyanovsk', 'ufa', 'chelyabinsk', 'yaroslavl']
+from ui import Ui_MainWindow
+from cities import city
 
 
-# Функция вычисления кол-ва страниц при пагинации
-def numbers_of_pages(city):
-    address = f'https://prodoctorov.ru/{city}/lpu/?page=1'
+# Класс парсера
+class Parce(QtWidgets.QMainWindow):
 
-    response = requests.get(url=address, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.init_ui()
+        self.ua = UserAgent()
+        self.ui.comboBox.addItems(city.keys())
+        self.ui.pushButton.clicked.connect(self.on_click)
 
-    total = soup.find('h1', class_='p-page__title')['data-counter']
+    def init_ui(self):
+        self.setWindowTitle('ПроДокторов')
 
-    total = total.replace('(', '').replace(')', '').replace(' ', '')
+    def on_click(self):
+        self.collect_data(city[self.ui.comboBox.currentText()])
 
-    return math.ceil(int(total) / 20)
+    def get_headers(self):
 
+        headers = {
+            'user-agent': self.ua.random,
+            'accept': 'application/json, text/plain, */*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        }
 
-def collect_data(city):
-    # Создание файла с результатами парсинга
-    with open(f'{city}_res.csv', 'w', encoding='utf-8-sig', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(['Наименование клиники', 'Направление', 'Номер телефона'])
+        return headers
 
-    amount = numbers_of_pages(city)
+    # Функция вычисления кол-ва страниц при пагинации
+    def numbers_of_pages(self, city):
+        address = f'https://prodoctorov.ru/{city}/lpu/?page=1'
 
-    for page in range(1, amount + 1):
-        # Отправка запроса на сайт
-        response = requests.get(url=f'https://prodoctorov.ru/{city}/lpu/?page={page}', headers=headers)
-        response.encoding = 'utf-8'
-
-        # Парсинг отдельной страницы
+        response = requests.get(url=address, headers=self.get_headers())
         soup = BeautifulSoup(response.text, 'lxml')
 
-        cards = soup.find('div', class_='appointments_page b-container').find_all('div', class_='b-card__top')
+        total = soup.find('h1', class_='p-page__title')['data-counter']
 
-        for card in cards:
+        total = total.replace('(', '').replace(')', '').replace(' ', '')
 
-            name = card.find('a', class_='b-card__name-link b-link ui-text ui-text_h5 ui-text_color_primary-blue')\
-                .text.strip()
-            category = card.find('div', class_='b-card__category ui-text ui-text_body-1 ui-text_color_deep-grey')\
-                .text.strip()
+        return math.ceil(int(total) / 20)
 
-            try:
-                phone = '+7 ' + card.find('span', {'class': 'b-card__lpu-phone-num'}).text
-            except AttributeError:
-                phone = 'Телефона нет'
+    def collect_data(self, city):
 
-            # Запись результатов парсинга
-            with open(f'{city}_res.csv', 'a', encoding='utf-8-sig', newline='') as file:
-                writer = csv.writer(file, delimiter=';')
-                writer.writerow([name, category, phone])
+        # Создание файла с результатами парсинга
+        with open(f'{city}_res.csv', 'w', encoding='utf-8-sig', newline='') as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow(['Наименование клиники', 'Направление', 'Номер телефона'])
 
-    print('Файл записан')
+        amount = self.numbers_of_pages(city)
+
+        for page in range(1, amount + 1):
+            # Отправка запроса на сайт
+            response = requests.get(url=f'https://prodoctorov.ru/{city}/lpu/?page={page}', headers=self.get_headers())
+            response.encoding = 'utf-8'
+
+            # Парсинг отдельной страницы
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            cards = soup.find('div', class_='appointments_page b-container').find_all('div', class_='b-card__top')
+
+            for card in cards:
+
+                name = card.find('a', class_='b-card__name-link b-link ui-text ui-text_h5 ui-text_color_primary-blue')\
+                    .text.strip()
+                category = card.find('div', class_='b-card__category ui-text ui-text_body-1 ui-text_color_deep-grey')\
+                    .text.strip()
+
+                try:
+                    phone = '+7 ' + card.find('span', {'class': 'b-card__lpu-phone-num'}).text
+                except AttributeError:
+                    phone = 'Телефона нет'
+
+                # Запись результатов парсинга
+                with open(f'{city}_res.csv', 'a', encoding='utf-8-sig', newline='') as file:
+                    writer = csv.writer(file, delimiter=';')
+                    writer.writerow([name, category, phone])
+
+        print('Файл записан')
 
 
 def main():
-    collect_data('tolyatti')
+    app = QtWidgets.QApplication([])
+    application = Parce()
+    application.show()
+
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
